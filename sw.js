4e78f013f7828035627ec05773d729c88d9b5dbb
Abu Basim <abu-basim@example.com>
2026-09-18 02:06:57 +0300

    تحويل الموقع لتطبيق PWA وإنشاء واجهة المواد

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'randa-pwa-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['./', './index.html', './manifest.json', './assets/icon.png']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
