bf11225db4cc4bf983cbc3074fc448f08447b8a6
Abu Basim <abu-basim@example.com>
2026-09-18 22:25:59 +0300

    إضافة التبديل التلقائي الاحتياطي لتجاوز ضغط السيرفرات

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1789759374';
+const CACHE_VERSION = '1789759559';
 const CACHE_NAME = 'randa-pwa-v' + CACHE_VERSION;
 const urlsToCache = ['./', './index.html', './manifest.json', './arabic_lesson1.html'];
 
