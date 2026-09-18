e6b635670a8f85385c5df71f962949cc566047db
Abu Basim <abu-basim@example.com>
2026-09-18 03:44:30 +0300

    إضافة لوحة تحكم النخبة وإعدادات الذكاء الاصطناعي

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1789690245';
+const CACHE_VERSION = '1789692270';
 const CACHE_NAME = 'randa-pwa-v' + CACHE_VERSION;
 const urlsToCache = ['./', './index.html', './manifest.json', './arabic_lesson1.html'];
 
