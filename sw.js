1d7122566fa9af935500a0d54c967e9d5e2067da
Abu Basim <abu-basim@example.com>
2026-09-18 22:08:48 +0300

    معالجة التكرار عند الإلغاء وتوسيع شرح الذكاء الاصطناعي الأكاديمي

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1789757994';
+const CACHE_VERSION = '1789758528';
 const CACHE_NAME = 'randa-pwa-v' + CACHE_VERSION;
 const urlsToCache = ['./', './index.html', './manifest.json', './arabic_lesson1.html'];
 
