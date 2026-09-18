b0168497b8aeb042a1781d55170c6b79c6af2fa2
Abu Basim <abu-basim@example.com>
2026-09-18 23:27:49 +0300

    ربط المنصة سحابياً بـ Firebase Realtime Database للمزامنة التلقائية

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1789762052';
+const CACHE_VERSION = '1789763269';
 const CACHE_NAME = 'randa-pwa-v' + CACHE_VERSION;
 const urlsToCache = ['./', './index.html', './manifest.json', './arabic_lesson1.html'];
 
